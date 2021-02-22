c909bcc5147c885e6094fa347548e4557f722499
whynot <anil@panix.com>
2021-02-22 11:54:59 -0500

    Small Changes

--- code/customautoconfig/SpecialPrinterAutoConfig/src/main/java/ttl/larku/specialprinter/SpecialPrinterAutoConfig.java
+++ code/customautoconfig/SpecialPrinterAutoConfig/src/main/java/ttl/larku/specialprinter/SpecialPrinterAutoConfig.java
@@ -1,6 +1,7 @@
 package ttl.larku.specialprinter;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
