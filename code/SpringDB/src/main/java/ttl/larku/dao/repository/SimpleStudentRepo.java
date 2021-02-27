93fc506328f72425cf0cc121c35a356cd9cdef3a
whynot <anil@panix.com>
2021-02-27 10:23:16 -0500

    Final submit

--- code/SpringDB/src/main/java/ttl/larku/dao/repository/SimpleStudentRepo.java
+++ code/SpringDB/src/main/java/ttl/larku/dao/repository/SimpleStudentRepo.java
@@ -5,9 +5,15 @@ import org.springframework.stereotype.Repository;
 import ttl.larku.domain.Student;
 
 /**
- * This is the simplest JpaRepository.  Gives you all the basic
- * crud methods:
+ * This is the simplest JpaRepository.  Gives you all the basic crud methods:
  * https://docs.spring.io/spring-data/jpa/docs/current/api/org/springframework/data/jpa/repository/JpaRepository.html
+ *
+ * Basic idea behind a repository is that you create an interface which extends the appropriate Spring
+ * Repository interface (In this case the JpaRepository).  Spring will then create the implementing
+ * class for you.
+ *
+ * Here we are creating a Repository for the Student class.  The type of the primary
+ * key in Student is Integer.
  *
  * Example of usage in StudentRepoService.
  *
